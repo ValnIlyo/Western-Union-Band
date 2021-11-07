cb5c8a95907e26d4605dfeb96fafc85fe5e074b8
Vivian Alexander Lyngdoh Nonglait <92193584+ValnIlyo@users.noreply.github.com>
2021-11-07 17:59:09 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,74 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [{
+        name: "Howzat",
+        src: "music-1"
+    },
+
+    {
+        name: "Did You Boogie (With Your Baby)",
+        src: "music-2"
+    },
+    {
+        name: "Movie Star",
+        src: "music-3"
+    },
+    {
+        name: "Wise Man",
+        src: "music-4"
+    },
+    {
+        name: "We All Fall In Love Sometimes",
+        src: "music-5"
+    },
+    {
+        name: "Needles And Pins",
+        src: "music-6"
+    }, {
+        name: "Come Together",
+        src: "music-7"
+    },
+    {
+        name: "Living Next Door To Alice",
+        src: "music-8"
+    },
+    {
+        name: "Nights Are Forever",
+        src: "music-9"
+    },
+    {
+        name: "First Cut Is The Deepest",
+        src: "music-10"
+    },
+    {
+        name: "Paper Roses, Paper Dreams",
+        src: "music-11"
+    }, {
+        name: "Slipping Away",
+        src: "music-12"
+    },
+    {
+        name: "Stand By Me",
+        src: "music-13"
+    },
+    {
+        name: "Sausolito",
+        src: "music-14"
+    }
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+];
